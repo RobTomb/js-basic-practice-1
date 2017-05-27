014e54178a4be98902c4bcf6377226c69da9cc28
robtomb <robtomb@outlook.com>
2017-05-27 14:56:32 +0800

    the chance i try to do maby err

--- main/main.js
+++ main/main.js
@@ -1,9 +1,8 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(divisor,dividend) {return divisor%dividend};
 
-function caclSum() {};
-
-function caclSumInConditon() {};
+function caclSum(numArray) {"use strict"; let sum=0; numArray.forEach(function(item){ sum+=item; }); return sum; }
+function caclSumInConditon(numArray) { 'use strict'; let sum = 0 ; m};
 
 module.exports = {
     caclRemaindar,
